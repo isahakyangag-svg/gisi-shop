bb508df57c46270b8ecec8a0b22bab8aae1f7ec3
isahakyangag-svg <isahakyangag@gmail.com>
2025-12-17 13:05:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function filterProducts(category) {
+    const products = document.querySelectorAll('.product');
+    products.forEach(product => {
+        if (category === 'all' || product.dataset.category === category) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
+}
